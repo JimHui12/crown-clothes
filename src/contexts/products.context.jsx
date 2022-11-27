97af24b54b3fae07b1ac87131fcbde85d9b2d8b3
JimHui12 <jianghua.hui@uq.net.au>
2022-11-27 18:06:07 +1000

    in useEffect, async await needs to be made with new function

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,17 +1,27 @@
-import React, {useState, useEffect, createContext} from "react"
+import React, { useState, useEffect, createContext } from "react";
 
-import {addCollectionAndDocuments} from '../utils/firebase/firebase.utils'
-
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
-    products: [],
-})
+  products: [],
+});
 
-export const ProductsProvider = ({children})=> {
-    const [products, setProducts]  = useState([])
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
 
-    const value = {products}
-    return (
-        <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
-    )
-}
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+       const categoryMap = await getCategoriesAndDocuments()
+       console.log(categoryMap)
+
+    }
+    getCategoriesMap()
+  }, [])
+
+  const value = { products };
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -17,7 +17,9 @@ import {
   getDoc,
   setDoc,
   collection,
-  writeBatch
+  writeBatch,
+  query,
+  getDocs
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -57,6 +59,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   })
   await batch.commit()
   console.log('done')
+}
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories')
+  const q = query(collectionRef)
+
+  const querySnapshot = await getDocs(q)
+  const categoryMap =  querySnapshot.docs.reduce((acc, docSnapShot) => {
+    const  { title, items} = docSnapShot.data()
+    acc[title.toLowerCase()] = items
+    return acc
+  }, {})
+
+  return categoryMap
+
 }
 
 export const createUserDocumentFromAuth = async (
