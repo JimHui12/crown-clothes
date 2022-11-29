caadfbbf0d6bcb6834220cf39453ce21da5ce65c
JimHui12 <jianghua.hui@uq.net.au>
2022-11-29 10:42:18 +1000

    styled-component added to button component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,9 @@
 import React from "react"
 import { useState } from "react"
 import FormInput from "../form-input/form-input.component"
-import Button from "../button/button.component"
+import Button, {BUTTON_TYPES_CLASSES} from "../button/button.component"
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword
 } from "../../utils/firebase/firebase.utils";
 
@@ -76,7 +75,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button type="button" buttonType={BUTTON_TYPES_CLASSES.google} onClick={signInWithGoogle}>
             Google sign in
           </Button>
         </div>
