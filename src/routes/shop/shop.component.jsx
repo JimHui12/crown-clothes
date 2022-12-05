189cf856b4224cff2ea139f362138ed1284f5f70
JimHui12 <jianghua.hui@uq.net.au>
2022-12-05 15:53:45 +1000

    convert categories context to redux

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom'
+import { useDispatch } from 'react-redux';
 import CategoryPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
+import {setCategoriesMap } from '../../store/categories/categories.action'
 import "./shop.styles.scss";
 
 const Shop = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+       const categoryMap = await getCategoriesAndDocuments('categories')
+       dispatch(setCategoriesMap(categoryMap))
+    }
+    getCategoriesMap()
+  }, [])
  
   return (
     <Routes>
