2055e07900a3c59ed747b3d48d504fb4f632c064
JimHui12 <jianghua.hui@uq.net.au>
2022-12-06 16:57:14 +1000

    migrate cart context to redux

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,9 +1,17 @@
 import { createSelector } from "reselect";
 
-export const selectCategoriesMap = (state) => {
-  return state.categories.categories.reduce((acc, category) => {
+const selectCategoriesReducer = state => state.categories
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories], 
+  categories => categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {});
-}
+  }, {})
+)
