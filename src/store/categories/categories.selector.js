29c89596c371d6998dba0bc17dbd400eac255d85
JimHui12 <jianghua.hui@uq.net.au>
2022-12-06 11:17:25 +1000

    setup reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,9 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+export const selectCategoriesMap = (state) => {
+  return state.categories.categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
   }, {});
+}
