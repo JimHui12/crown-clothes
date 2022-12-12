3fdb563900cc7605814b8e1378d273f074041a9e
JimHui12 <jianghua.hui@uq.net.au>
2022-12-12 10:55:45 +1000

    check in user session saga

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import { Routes, Route } from "react-router-dom"
 import {useEffect} from "react";
 import { useDispatch } from 'react-redux';
 
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-  getCurrentUser,
-} from "./utils/firebase/firebase.utils";
-
 import Navigation from "./routes/navigation/navigation.component"
 import Home from "./routes/home/home.component"
 import Authentication from "./routes/authentication/authentication.component"
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
-import { setCurrentUser } from './store/user/user.action'
+import { checkUserSession } from './store/user/user.action'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    getCurrentUser().then(user => console.log(user))
+    dispatch(checkUserSession())
   }, []);
 
   return (
--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects'
 
 import { categoriesSaga } from './categories/categories.saga'
+import { userSagas } from './user/user.saga';
 
 export function* rootSaga() {
-    yield all([(categoriesSaga)]);
+    yield all([(categoriesSaga), call(userSagas)]);
 }
