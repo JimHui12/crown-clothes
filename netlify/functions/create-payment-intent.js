084e00ffa891bec70855c1c85bd487f1df8acb0b
JimHui12 <jianghua.hui@uq.net.au>
2022-12-16 11:15:31 +1000

    hooked to stripe

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async event => {
     try {
@@ -7,7 +7,7 @@ exports.handler = async event => {
         const paymentIntent = await stripe.paymentIntents.create({
             amount,
             currency: "usd",
-            payment_method_types: ["cards"]
+            payment_method_types: ["card"]
         })
 
         return {
